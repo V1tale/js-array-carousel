31665d54482d6c23f6d1127457849682883d86fb
V1tale <elativ.c@gmail.com>
2022-10-12 19:41:42 +0200

    completate funzionalità

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ for (let i = 0; i < pictures.length; i++) {
 const dynamicElement = `<div class="picture">
                         <img src="${pictures[i]}" alt="">
                         </div>`;
-content.innerHTML = dynamicElement;
+content.innerHTML += dynamicElement;
 console.log(dynamicElement);
 }
 // RENDERE VISIBILE LA FOTO SELEZIONATA
@@ -20,13 +20,33 @@ console.log(selected);
 images[selected].classList.add("visible");
 console.log(images);
 // BOTTONE DI SCORRIMENTO
-// SCORRIMENTO IN AVANTI
+
+// BUTTON FORWARD
 const downButton = document.getElementById("chevron-down");
 downButton.addEventListener ("click", function() {
  if (selected < (pictures.length -1 )) {
     images[selected].classList.remove("visible");
     selected++;
     images[selected].classList.add("visible");
+   }
+   else if (selected === (pictures.length -1)) {     
+      images[selected].classList.remove("visible");
+      selected = 0;
+      images[selected].classList.add("visible");
+   }
+})
 
+// BUTTON BACKWARD 
+const upButton = document.getElementById("chevron-up");
+upButton.addEventListener ("click", function() {
+ if (selected > 0 ) {
+    images[selected].classList.remove("visible");
+    selected--;
+    images[selected].classList.add("visible");
  }
+ else if (selected === 0) {     
+   images[selected].classList.remove("visible");
+   selected = (pictures.length -1);
+   images[selected].classList.add("visible");
+}
 })
